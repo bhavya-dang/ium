4e09f89623eeec1a3a70b6f38e7461e22afe109b
tetraaaaa <tetragamingtg@gmail.com>
2018-03-16 20:22:25 -0700

    Invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
-    message.channel.send("**Invite ium **https://discordapp.com/oauth2/authorize?client_id=421511226015612929&scope=bot&permissions=2146958591");
+    message.channel.send("**Invite ium **https://discordapp.com/oauth2/authorize?client_id=421511226015612929&scope=bot&permissions=805314622");
 }
