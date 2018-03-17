7530e1d2082941d1853fd3d03d0c45b2bb2057cb
tetraaaaa <tetragamingtg@gmail.com>
2018-03-16 21:26:28 -0700

    Github Command

--- commands/github.js
+++ commands/github.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
     let githubEmbed = new Discord.RichEmbed()
     .setColor('#ffffff ')
-    .addField("Github", "ium")
+    .addField("Github", "https://github.com/tetra-dev/ium")
 
     message.channel.send(githubEmbed);
 }
