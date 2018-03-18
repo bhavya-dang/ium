e5d7d5d7f7af6d0b18ffe6f7ba14d5ca59225077
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 16:42:36 -0700

    qerer

--- commands/clear.js
+++ commands/clear.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
 
-  //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**You do not have permission to do that.**");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**You do not have permission to do that.**");
   if(!args[0]) return message.channel.send("Clear Command: **ium clear [amount of messages to clearn]**");
   message.channel.bulkDelete(args[0]).then(() => {
   message.delete().catch(O_o=>{});
