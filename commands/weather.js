0a989974575f75e7672fb39caa24aaff5ae75149
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 10:31:25 -0700

    Weather.js 2

--- commands/weather.js
+++ commands/weather.js
@@ -3,31 +3,33 @@ const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
 
-    weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-        if(err) message/channel.send(err);
+    weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) { // Make sure you get that args.join part, since it adds everything after weather.
+        if (err) message.channel.send(err);
 
-        message.channel.send(JSON.stringify(result[0].current, null, 2));
-
+        // We also want them to know if a place they enter is invalid.
         if (result.length === 0) {
-            message.channel.send(`**Please enter a valid location.**`)
+            message.channel.send('**Please enter a valid location.**') // This tells them in chat that the place they entered is invalid.
+            return; // This exits the code so the rest doesn't run.
         }
-    
 
-    var current = result[0].current;
-    var location = result[0].location;
+        // Variables
+        var current = result[0].current; // This is a variable for the current part of the JSON output
+        var location = result[0].location; // This is a variable for the location part of the JSON output
 
-    const weatherEmbed = new Discord.RichEmbed()
-    .setColor('#f5a3fa')
-    .setDescription(`**${current.skytext}**`)
-    .setAuthor(`Weather for ${current.observationpoint}`)
-    .setThumbnail(current.imageURL)
-    .addField('Timezone', `UTC${location.timezone}`, true)
-    .addField('Degree Type', location.degreetype, true)
-    .addField('Temperature', `${current.temperature} Degrees`, true)
-    .addField('Feels Like', `${current.feelslike} Degrees`, true)
-    .addField("Winds", current.winddisplay, true)
-    .addField("Humidity", `${current.humidity}%`, true);
+        // Let's use an embed for this.
+        const weatherEmbed = new Discord.RichEmbed()
+            .setDescription(`**${current.skytext}**`) // This is the text of what the sky looks like, remember you can find all of this on the weather-js npm page.
+            .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
+            .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
+            .setColor("#f5a3fa") // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
+            .addField('Timezone',`UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
+            .addField('Degree Type',location.degreetype, true)// This is the field that shows the degree type, and is inline
+            .addField('Temperature',`${current.temperature} Degrees`, true)
+            .addField('Feels Like', `${current.feelslike} Degrees`, true)
+            .addField('Winds',current.winddisplay, true)
+            .addField('Humidity', `${current.humidity}%`, true)
 
-    message.channel.send(weatherEmbed);
-});
+            // Now, let's display it when called
+            message.channel.send({weatherEmbed});
+    });
 }
