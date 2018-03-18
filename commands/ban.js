1373b373d2aa55f32c50c226462e844dbe5f8fbe
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 13:09:16 -0700

    ewrqe

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 
-module.exports.run = async (bot, message, args) => {
+exports.run = async (bot, message, args) => {
 
 if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**You do not have permission to do that.**");
     
@@ -9,10 +9,14 @@ if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("*
       return message.reply("**Please mention member to ban!**");
     if(!member.bannable) 
       return message.reply("**I cannot ban this user!**");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
     
-    await member.ban()
+    await member.ban(reason)
       .catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.username} has been banned by ${message.author.username}.`);
+    message.reply(`${member.user.username} has been banned by ${message.author.username} because: **__${reason}__**`);
 
     let Banembed = new Discord.RichEmbed()
     .setColor("#ecf0f1")
@@ -20,9 +24,8 @@ if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("*
     .addField('Action:', 'Ban')
     .addField('User:', `${member.user.username}#${member.user.discriminator} (${member.user.id})`)
     .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
 
     message.channel.send(Banembed);
-
-    message.delete();
 
 }
