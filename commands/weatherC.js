48a3cf6d8942162d3c99b10b61dbae0c563d546b
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 16:50:32 -0700

    F, and C

---
+++ commands/weatherC.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const weather = require('weather-js')
+exports.run = (bot, message, args) => {
+    
+    weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) { 
+    //   if (err) message.channel.send(err);
+      if (result === undefined || result.length === 0) {
+          const embed = new Discord.RichEmbed()
+          .setAuthor("Please enter a valid location")
+          .setColor("#f5a3fa")
+          return message.channel.send(embed);
+      }
+        var current = result[0].current; 
+        var location = result[0].location; 
+
+        const embed = new Discord.RichEmbed()
+            .setDescription(`**${current.skytext}**`)
+            .setThumbnail(current.imageUrl) 
+            .setAuthor(`Weather for ${current.observationpoint}`) 
+            .setColor("#f5a3fa") 
+            .addField('Timezone', `UTC${location.timezone}`, true) 
+            .addField('Degree Type', location.degreetype, true) 
+            .addField('Temperature', `${current.temperature} Degrees`, true)
+            .addField('Feels Like', `${current.feelslike} Degrees`, true)
+            .addField('Winds', current.winddisplay, true)
+            .addField('Humidity', `${current.humidity}%`, true)
+        message.channel.send(embed)
+    })
+ } 
