1a8fb9074b7853c9000a654d0ac7be2c7ce33e40
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 13:03:05 -0700

    ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,18 +1,28 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
 
-exports.run = async (bot, message, args) => {
-    let banUser = message.mentions.members.first();
-    if(!banUser) message.reply("**User not found.**");
-    if(!message.member.hasPermission("MANNAGE_MESSAGES")) return message.reply("**You do not have permission to do that!**");
-    if(banUser.hasPermission("MANNAGE_MESSAGES")) return message.reply("**You cannot ban that user!**");
+module.exports.run = async (bot, message, args) => {
 
-    let banEmbed = new Discord.RichEmbed()
-    .setDescription("Ban")
-    .setColor("#ecf0f1")
-    .addField("Banned User", `${banUser}`)
-    .addField("Banned By", `<@${message.author.id}>`);
-    message.guild.member();
-    banUser.ban();
+if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**You do not have permission to do that.**");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("**Please mention member to ban!**");
+    if(!member.bannable) 
+      return message.reply("**I cannot ban this user!**");
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.username} has been banned by ${message.author.username}.`);
 
-    return message.channel.send(banEmbed);
+    let Banembed = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .setTimestamp()
+    .addField('Action:', 'Ban')
+    .addField('User:', `${member.user.username}#${member.user.discriminator} (${member.user.id})`)
+    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+
+    message.channel.send(Banembed);
+
+    message.delete();
+
 }
--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
     .setColor('#ffffff')
     .addField("Commands", "Prefix: **ium **")
     .addField("Info", "discord, invite, server, info, ping, help, dev, version, definition, avatar")
-    .addField("Moderation", "ban, kick, mute, addrole, removerole, report, warn, warnlevel (malfunctioning)")
+    .addField("Moderation", "ban, kick, mute, report, warn, warnlevel (malfunctioning)")
     .addField("Fun", "dog, 8ball, choose, say, scale")
     .addField("ium", "We are constantly adding more and more commands to make this bot much better.");
 
