4abe83885c4e29306af5d642665ddaaa4a616b8d
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 11:50:35 -0700

    ee

--- commands/weather.js
+++ commands/weather.js
@@ -1,37 +1,29 @@
-const weather = require('weather.js');
-const Discord = require("discord.js");
+const Discord = require('discord.js')
+const weather = require('weather-js')
+exports.run = (client, message, args) => {
+    
+    weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) { 
+    //   if (err) message.channel.send(err);
+      if (result === undefined || result.length === 0) {
+          const embed = new Discord.MessageEmbed()
+          .setAuthor("Please enter a valid location")
+          .setColor("RANDOM")
+          return message.channel.send(embed);
+      }
+        var current = result[0].current; 
+        var location = result[0].location; 
 
-exports.run = async (bot, message, args) => {
-
-    weather({search: args.join(" "), degreeType: 'F'}, function(err, result) { 
-        if (err) message.channel.send(err);
-
-
-        if (result.length === 0) {
-            message.channel.send('**Please enter a valid location.**') 
-            return;
-        }
-
-        // Variables
-        let current = result[0].current; 
-        let location = result[0].location; 
-
-        const weatherEmbed = new Discord.RichEmbed()
-            .setDescription(`**${current.skytext}**`) 
-            .setAuthor(`Weather for ${current.observationpoint}`) 
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`**${current.skytext}**`)
             .setThumbnail(current.imageUrl) 
-            .setColor("#f5a3fa") 
-            .addField('Timezone',`UTC${location.timezone}`, true) 
-            .addField('Degree Type',location.degreetype, true)
-            .addField('Temperature',`${current.temperature} Degrees`, true)
+            .setAuthor(`Weather for ${current.observationpoint}`) 
+            .setColor("RANDOM") 
+            .addField('Timezone', `UTC${location.timezone}`, true) 
+            .addField('Degree Type', location.degreetype, true) 
+            .addField('Temperature', `${current.temperature} Degrees`, true)
             .addField('Feels Like', `${current.feelslike} Degrees`, true)
-            .addField('Winds',current.winddisplay, true)
+            .addField('Winds', current.winddisplay, true)
             .addField('Humidity', `${current.humidity}%`, true)
-
-            message.channel.send({weatherEmbed});
-    }); 
-}
-
-module.exports.help = {
-    name: weather
-}
+        message.channel.send(embed)
+    })
+ }
