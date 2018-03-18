f8548c8acc2063e165dfc3c5e6f8b2bd5d8ad40e
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 12:13:39 -0700

    qeew

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
-const client = new Discord.Client({disableEveryone: true});
 //let iumicDollar = require("/.money.json");
 
 const newUsers = new Discord.Collection();
