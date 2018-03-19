d815bb4779bae0e60feb22f4c0cccd592fe46fe1
tetraaaaa <tetragamingtg@gmail.com>
2018-03-18 17:58:48 -0700

    ewrq

--- index.js
+++ index.js
@@ -59,4 +59,20 @@ bot.on("message", message => {
 });
 
 
+//Glitch.com stuff
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
 bot.login(process.env.BOT_TOKEN);
