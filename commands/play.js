04bdda93cb65645035cd697eec6c8422021bab1b
tetraaaaa <tetragamingtg@gmail.com>
2018-03-20 21:27:30 -0700

    user

--- commands/play.js
+++ commands/play.js
@@ -4,13 +4,7 @@ const ytdl = require('ytdl-core');
 exports.run = async (bot, message, args) => {
     const voiceChannel = message.member.voiceChannel;
 		if (!voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-		const permissions = voiceChannel.permissionsFor(message.bot.user);
-		if (!permissions.has('CONNECT')) {
-			return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
-		}
-		if (!permissions.has('SPEAK')) {
-			return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
-        }
+		
         
         try {
             var connection = await voiceChannel.join();
