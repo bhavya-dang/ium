eeae432d7e1cb12aef3ebc6faa5183cb43bfd8f6
tetraaaaa <tetragamingtg@gmail.com>
2018-03-22 17:20:57 -0700

    msg -> message

--- commands/play.js
+++ commands/play.js
@@ -3,18 +3,18 @@ const ytdl = require('ytdl-core');
 const queue = new Map();
 
 exports.run = async (bot, message, args) => {
-    const serverQueue = queue.get(msg.guild.id);
+    const serverQueue = queue.get(message.guild.id);
     const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 
     const voiceChannel = message.member.voiceChannel;
 		if (!voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
         
-        const permissions = voiceChannel.permissionsFor(msg.client.user);
+        const permissions = voiceChannel.permissionsFor(message.client.user);
 		if (!permissions.has('CONNECT')) {
-			return msg.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+			return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
 		}
 		if (!permissions.has('SPEAK')) {
-			return msg.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+			return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
         }
         
         const songInfo = await ytdl.getInfo(args[1]);
@@ -28,31 +28,31 @@ exports.run = async (bot, message, args) => {
 
         if (!serverQueue) {
             const queueConstruct = {
-                textChannel: msg.channel,
+                textChannel: message.channel,
                 voiceChannel: voiceChannel,
                 connection: null,
                 songs: [],
                 volume: 5,
                 playing: true
             };
-            queue.set(msg.guild.id, queueConstruct);
+            queue.set(message.guild.id, queueConstruct);
     
             queueConstruct.songs.push(song);
     
             try {
                 var connection = await voiceChannel.join();
                 queueConstruct.connection = connection;
-                play(msg.guild, queueConstruct.songs[0]);
+                play(message.guild, queueConstruct.songs[0]);
             } catch (error) {
                 console.error(`I could not join the voice channel: ${error}`);
-                queue.delete(msg.guild.id);
-                return msg.channel.send(`I could not join the voice channel: ${error}`);
+                queue.delete(message.guild.id);
+                return message.channel.send(`I could not join the voice channel: ${error}`);
             }
 
         } else {
             serverQueue.songs.push(song);
             console.log(serverQueue.songs);
-            return msg.channel.send(`✅ **${song.title}** has been added to the queue!`);
+            return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
         }
         return;
 
