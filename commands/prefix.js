865cf6bea511a91afe742008ccf2da53d8ccdd2a
tetraaaaa <tetragamingtg@gmail.com>
2018-03-25 15:47:15 -0700

    prefix.json

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args, prefix) => {
     if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
   if(!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <desired prefix here>`);
 
-  let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
   prefixes[message.guild.id] = {
     prefixes: args[0]
