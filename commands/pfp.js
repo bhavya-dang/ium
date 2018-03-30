a8dada378e1d6561ad070d9a0a5a52d7083af86a
tetraaaaa <tetragamingtg@gmail.com>
2018-03-29 21:29:04 -0700

    I dont know what im doing...

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
 
 exports.run = async (bot, message, args) => {
     let msg = await message.channel.send("Generating avatar...");
-    if(!message.guild.iconURL) return msg.edit("No icon found!");
-    let target = message.mentions.users.first();
+    let target = message.mentions.users.first() || message.author;
 
-    let avatarEmbed = new Discord.RichEmbed()
-    .setTitle(`${target}'s avatar`)
-    .setColor("#FFFFFF")
-    .setImage(message.guild.iconURL)
-    .setDescription("[Icon URL link]("+message.guild.iconURL+")");
+    await message.channel.send({files: [
+        {
+        attachment: target.displayAvatarURL,
+        name: "avatar.png"
+        }
+    ]});
 
-    message.channel.send(avatarEmbed)
+    message.channel.send(iconembed)
     msg.delete();
 
 }
