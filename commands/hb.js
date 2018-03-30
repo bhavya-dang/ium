a59a6961366a275437aadf70dd8d5ef18b65bc39
tetraaaaa <tetragamingtg@gmail.com>
2018-03-29 21:26:02 -0700

    Cleaner Avatar.js

---
+++ commands/hb.js
@@ -0,0 +1,19 @@
+const hastebin = require('hastebin-gen');
+
+exports.run = (bot, message, args) => {
+
+     let haste = args.slice(0).join(" ")
+
+        let type = args.slice(1).join(" ")
+
+        if (!args[0]) { return message.channel.send("**You must paste some code to put in hastebin.**") }
+
+        hastebin(haste).then(r => {
+
+            message.channel.send("Ive posted your code on hastebin! " + r);
+
+        }).catch(console.error);
+
+        message.delete();
+
+}        
