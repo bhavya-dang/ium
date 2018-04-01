3b6569c828e3cdd990e0ab71c38dec71cdf30469
tetraaaaa <tetragamingtg@gmail.com>
2018-04-01 12:35:49 -0700

    Things have gone...

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,9 @@ exports.run = async (bot, message, args) => {
     const serverEmbed = new Discord.RichEmbed()
     .setColor('#ffffff')
     .addField("Commands", "Prefix: **ium **")
-    .addField("Info", "define, avatar, weather, announce")
+    .addField("Info", "define, avatar, weather, announce, hastebin, translate")
     .addField("Bot Info", "discord, invite, info, ping, dev, version, ium, stats")
-    .addField("Moderation", "ban, kick, mute, unmute, report, hastebin")
+    .addField("Moderation", "ban, kick, mute, unmute, report")
     .addField("Fun", "dog, 8ball, choose, say, scale, search, money, pay, mock, reverse, furry, bond, wave, rate")
     .addField("ium", "We are constantly adding more and more commands to make this bot much better.");
 
