e9c2a019cbc0c1ab108d9f7b550926ee225f3cfe
tetraaaaa <tetragamingtg@gmail.com>
2018-04-01 14:41:40 -0700

    profile command

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,11 @@ exports.run = async (bot, message, args) => {
     const serverEmbed = new Discord.RichEmbed()
     .setColor('#ffffff')
     .addField("Commands", "Prefix: **ium **")
-    .addField("Info", "define, avatar, weather, announce, hastebin, translate")
-    .addField("Bot Info", "discord, invite, info, ping, dev, version, ium, stats")
+    .addField("Info", "define, avatar, ping, xp")
+    .addField("Bot Info", "discord, invite, info, dev, version, ium, stats")
     .addField("Moderation", "ban, kick, mute, unmute, report")
-    .addField("Fun", "dog, 8ball, choose, say, scale, search, money, pay, mock, reverse, furry, bond, wave, rate")
+    .addField("Fun", "dog, 8ball, choose, say, money, pay, mock, reverse, furry, bond, wave, rate")
+    .addField("scale, search, announce, weather, hastebin, translate")
     .addField("ium", "We are constantly adding more and more commands to make this bot much better.");
 
     return message.channel.send(serverEmbed);
---
+++ commands/profile.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig");
+let xp = require("../xp.json");
+let iumics = require("../money.json");
+
+exports.run = async (bot, message, args) => {
+
+  if(!xp[message.author.id]){
+   xp[message.author.id] = {
+     xp: 0,
+     level: 1
+  };
+}
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 100;
+  let difference = nxtLvlXp - curxp;
+  let userIumics = iumics[message.author.id].iumics;
+
+  let target = message.mentions.users.first() || message.author;
+
+  let profileEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setThumbnail((target.displayAvatarURL))
+  .setColor("#FFFFFF")
+  .addField("Level", curlvl, true)
+  .addField("XP", curxp, true)
+  .addField("iumics", `**${userIumics}** iumics`, true)
+  .setFooter(`${difference} XP till level up`, message.author.displayAvatarURL);
+
+  message.channel.send(profileEmbed);
+
+}
